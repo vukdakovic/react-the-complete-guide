f9f3c94ace108edda2bbda6fee41ffabbba2501c
Crayzeyyy Vuk Dakovic <vuk.dakovic1992@gmail.com>
2020-03-05 23:01:00 +0100

    Izbrisao sam

--- React - The Complete Guide/assignment/src/UserInput/UserInput.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import './UserInput.css'
-
-const userInput = (props) => {
-  return <input className="UserInput" type="text" onChange={props.changed} value={props.name}/>
-}
-
-export default userInput;
